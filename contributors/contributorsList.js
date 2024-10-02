c7aa4937a0780d24e29e83975197a600b98b168a
sakettt25 <157049170+sakettt25@users.noreply.github.com>
2024-10-02 12:11:16 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Saket Saurav",
+    username: "https://github.com/sakettt25",
+  },
   
 ];
